672aba405f8caceadd640cdec5bedb37c5dbfcf5
Mary Rose Apoyon <maryroseapoyon1@gmail.com>
2018-01-20 02:31:30 +0900

    initial commit

---
+++ form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<form action="" method="get">
+<p>Login</p>
+<p>Username:
+<br/><input type="text" name="username"/></p>
+<p>Password:
+<br/><input type="password" name="password"/></p>
+<p>
+	<input type="submit" value="login"/>
+	<input type="reset" value="Clear"/>
+</p>
+</form>
+	
+
+</body>
+</html>
